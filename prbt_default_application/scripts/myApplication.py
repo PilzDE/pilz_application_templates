1d35a970dd3b2b6040e64f2c277b9e84873a988f
JonathanGruner <44464164+JonathanGruner@users.noreply.github.com>
2020-04-02 09:38:09 +0200

    Establish an standard for ROS Training
    Just changed the template to define the Robot positions as global variables.

--- prbt_default_application/scripts/myApplication.py
+++ prbt_default_application/scripts/myApplication.py
@@ -8,29 +8,29 @@ from tf.transformations import quaternion_from_euler
 __REQUIRED_API_VERSION__ = "1"    # API version
 __ROBOT_VELOCITY__ = 0.5          # velocity of the robot
 
+# important positions
+start_pos = [1.49, -0.54, 1.09, 0.05, 0.91, -1.67]   # joint values
+
+
+pick_pose = Pose(position=Point(0, -0.5, 0.25),  # cartesian coordinates
+                 orientation=quaternion_from_euler(
+                   0,
+                   math.radians(180),
+                   math.radians(0)))
+work_station_pose = Pose(position=Point(-0.5, 0.1, 0.2),
+                         orientation=quaternion_from_euler(
+                           0,
+                           math.radians(-135),
+                           math.radians(90)))
+place_pose = Pose(position=Point(-0.1, 0.4, 0.25),
+                  orientation=quaternion_from_euler(
+                    0,
+                    math.radians(180),
+                    math.radians(90)))
 
 # main program
 def start_program():
     rospy.loginfo("Program started")  # log
-
-    # important positions
-    start_pos = [1.49, -0.54, 1.09, 0.05, 0.91, -1.67]   # joint values
-
-    pick_pose = Pose(position=Point(0, -0.5, 0.25),  # cartesian coordinates
-                     orientation=quaternion_from_euler(
-                       0,
-                       math.radians(180),
-                       math.radians(0)))
-    work_station_pose = Pose(position=Point(-0.5, 0.1, 0.2),
-                             orientation=quaternion_from_euler(
-                               0,
-                               math.radians(-135),
-                               math.radians(90)))
-    place_pose = Pose(position=Point(-0.1, 0.4, 0.25),
-                      orientation=quaternion_from_euler(
-                        0,
-                        math.radians(180),
-                        math.radians(90)))
 
     # move to start point with joint values to avoid random trajectory
     r.move(Ptp(goal=start_pos, vel_scale=__ROBOT_VELOCITY__))
